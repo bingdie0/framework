864fb2ba35f51a06c0c76a7c7eb01c850943480b
WANGJ <2794405068@qq.com>
2018-09-04 09:57:40 +0800

    添加消息转换器

--- framework-core-business/src/main/java/com/my/framework/coverter/message/FrameworkMessageConverter.java
+++ framework-core-business/src/main/java/com/my/framework/coverter/message/FrameworkMessageConverter.java
@@ -4,11 +4,17 @@ import com.alibaba.fastjson.JSON;
 import com.my.framework.response.ResponseUtils;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 /**
  * @author: Mr.WangJie
@@ -17,17 +23,31 @@ import java.io.IOException;
 public class FrameworkMessageConverter<T> extends AbstractHttpMessageConverter<T> {
     @Override
     protected boolean supports(Class clazz) {
-        return false;
+        return true;
     }
 
     @Override
     protected T readInternal(Class clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
-        return null;
+        Charset charset = getContentTypeCharset(inputMessage.getHeaders().getContentType());
+        return (T) StreamUtils.copyToString(inputMessage.getBody(), charset);
     }
 
     @Override
     protected void writeInternal(T o, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-//        ResponseUtils.getSuccessResult(o);
-        System.out.println(1);
+        Object result = ResponseUtils.getSuccessResult(o);
+        String json = JSON.toJSONString(result);
+        OutputStream out = outputMessage.getBody();
+        out.write(json.getBytes());
+    }
+
+    private Charset getContentTypeCharset(@Nullable MediaType contentType) {
+        if (contentType != null && contentType.getCharset() != null) {
+            return contentType.getCharset();
+        }
+        else {
+            Charset charset = getDefaultCharset();
+            Assert.state(charset != null, "No default charset");
+            return charset;
+        }
     }
 }
--- framework-core-rest/src/main/java/com/my/framework/config/WebConfig.java
+++ framework-core-rest/src/main/java/com/my/framework/config/WebConfig.java
@@ -2,22 +2,30 @@ package com.my.framework.config;
 
 import com.my.framework.coverter.message.FrameworkMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author: Mr.WangJie
  * @date: 2018-09-03
  **/
-@EnableWebMvc
+
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new FrameworkMessageConverter<>());
+        FrameworkMessageConverter<Object> converter = new FrameworkMessageConverter<>();
+        List<MediaType> list = new ArrayList<>();
+        list.add(MediaType.TEXT_HTML);
+        list.add(MediaType.ALL);
+        converter.setSupportedMediaTypes(list);
+        converters.add(converter);
     }
 }
